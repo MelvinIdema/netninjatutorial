df7711b9c4de95766e2cd60a14c549d324ec46f0
Melvin Idema <melvinidema@melvins-mbp.home>
2021-05-22 14:45:34 +0200

    Clone lesson 10

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,7 @@
-import { createClient } from "contentful";
-import RecipeCard from "../components/RecipeCard";
-
-export async function getStaticProps() {
-    const client = createClient({
-        space: process.env.CONTENT_SPACE_ID,
-        accessToken: process.env.CONTENT_API_KEY
-    });
-    const res = await client.getEntries({ content_type: "recipe" });
-
-    return {
-        props: { recipes: res.items },
-        revalidate: 1
-    }
-}
-
-
-export default function Recipes({ recipes }) {
+export default function Recipes() {
   return (
     <div className="recipe-list">
-        {recipes.map(recipe => (
-            <RecipeCard key={recipe.sys.id} recipe={recipe} />
-        ))}
-        <style jsx>{`
-              .recipe-list {
-                display: grid;
-                grid-template-columns: 1fr 1fr;
-                grid-gap: 20px 60px;          
-              }
-        `}</style>
+      Recipe List
     </div>
   )
 }
